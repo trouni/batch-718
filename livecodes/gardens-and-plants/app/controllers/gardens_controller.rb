bb057c383bbc9dc2b6ffff8dd13d206418991060
Trouni Tiet <trouni@gmail.com>
2022-01-15 14:54:27 +0900

    rerun scaffold without jbuilder and add root route

--- livecodes/gardens-and-plants/app/controllers/gardens_controller.rb
+++ livecodes/gardens-and-plants/app/controllers/gardens_controller.rb
@@ -1,12 +1,12 @@
 class GardensController < ApplicationController
   before_action :set_garden, only: %i[ show edit update destroy ]
 
-  # GET /gardens or /gardens.json
+  # GET /gardens
   def index
     @gardens = Garden.all
   end
 
-  # GET /gardens/1 or /gardens/1.json
+  # GET /gardens/1
   def show
     @plant = Plant.new
   end
@@ -20,42 +20,30 @@ class GardensController < ApplicationController
   def edit
   end
 
-  # POST /gardens or /gardens.json
+  # POST /gardens
   def create
     @garden = Garden.new(garden_params)
 
-    respond_to do |format|
-      if @garden.save
-        format.html { redirect_to garden_url(@garden), notice: "Garden was successfully created." }
-        format.json { render :show, status: :created, location: @garden }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @garden.errors, status: :unprocessable_entity }
-      end
+    if @garden.save
+      redirect_to @garden, notice: 'Garden was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /gardens/1 or /gardens/1.json
+  # PATCH/PUT /gardens/1
   def update
-    respond_to do |format|
-      if @garden.update(garden_params)
-        format.html { redirect_to garden_url(@garden), notice: "Garden was successfully updated." }
-        format.json { render :show, status: :ok, location: @garden }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @garden.errors, status: :unprocessable_entity }
-      end
+    if @garden.update(garden_params)
+      redirect_to @garden, notice: 'Garden was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /gardens/1 or /gardens/1.json
+  # DELETE /gardens/1
   def destroy
     @garden.destroy
-
-    respond_to do |format|
-      format.html { redirect_to gardens_url, notice: "Garden was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to gardens_url, notice: 'Garden was successfully destroyed.'
   end
 
   private
--- livecodes/gardens-and-plants/config/routes.rb
+++ livecodes/gardens-and-plants/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'gardens#index'
   resources :gardens, only: [:index, :show] do
     resources :plants, only: :create
   end
